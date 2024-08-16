4a95f6c8686ee77db1e0e9d249f2885c3e2bdacc
Vatsal Khanna <vatsalkhanna1992@gmail.com>
2024-08-16 14:57:48 +0530

    POC of scrollama.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-$(document).foundation();
+//$(document).foundation();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scroller = scrollama();
+
+  const updateProgressPercentage = (percentage) => {
+    const percentageDisplay = document.querySelector('#progress-percentage');
+    percentageDisplay.textContent = `${Math.round(percentage * 100)}%`;
+  };
+
+  // Setup scrollama instance
+  scroller
+      .setup({
+          step: '.step',
+          offset: 0.5,
+          progress: true
+          //debug: true
+      })
+      .onStepEnter(response => {
+          const step = response.element;
+          // Add highlight to the current step
+          step.classList.add('highlight');
+      })
+      .onStepProgress(response => {
+          // Calculate the progress of the scroll
+          const progress = response.progress;
+          // Update progress bar
+          updateProgressPercentage(progress);
+      })
+      .onStepExit(response => {
+        const step = response.element;
+        // Add highlight to the current step
+        step.classList.remove('highlight');
+      });
+});
