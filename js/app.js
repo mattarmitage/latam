19eadc99ca0e0b1e602dfd4a82a8590084221a7c
Devhouse Websolutions <devhousewebsolutions@gmail.com>
2024-08-19 23:27:09 +0530

    IFS Scrollama.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,28 @@
-//$(document).foundation();
+jQuery(document).foundation();
 
 document.addEventListener('DOMContentLoaded', () => {
   const scroller = scrollama();
 
-  const updateProgressPercentage = (percentage) => {
-    const percentageDisplay = document.querySelector('#progress-percentage');
-    percentageDisplay.textContent = `${Math.round(percentage * 100)}%`;
-  };
-
   // Setup scrollama instance
   scroller
-      .setup({
-          step: '.step',
-          offset: 0.5,
-          progress: true
-          //debug: true
-      })
-      .onStepEnter(response => {
-          const step = response.element;
-          // Add highlight to the current step
-          step.classList.add('highlight');
-      })
-      .onStepProgress(response => {
-          // Calculate the progress of the scroll
-          const progress = response.progress;
-          // Update progress bar
-          updateProgressPercentage(progress);
-      })
-      .onStepExit(response => {
-        const step = response.element;
-        // Add highlight to the current step
-        step.classList.remove('highlight');
-      });
+    .setup({
+      step: '.step',
+      offset: 0.99,
+      //debug: true
+    })
+    .onStepEnter(response => {
+      const step = response.element;
+      if (step.id == 'step1' && step.hasEntered) {
+        document.querySelector('.footer .right-content').classList.remove('fade-out');
+        document.querySelector('.footer .right-content').classList.add('fade-in');
+      }
+      else if (step.id == 'step1' && !step.hasEntered) {
+        document.querySelector('.footer .right-content').classList.remove('fade-out');
+        step.hasEntered = true;
+      }
+      else {
+        document.querySelector('.footer .right-content').classList.remove('fade-in');
+        document.querySelector('.footer .right-content').classList.add('fade-out');
+      }
+    });
 });
