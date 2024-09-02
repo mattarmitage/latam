ac5605a4ef88e5fed5be08b8df3b90fb6be639c9
Ram <ramsinghsohal@gmail.com>
2024-09-02 23:05:12 +0200

    Dynamic city js

--- js/app.js
+++ js/app.js
@@ -23,32 +23,44 @@ document.addEventListener('DOMContentLoaded', () => {
   const scrollerBottomHandlers = {
     onStepEnter: ({ element: step, direction }) => {
       switch (step.id) {
-        case 'step-1-1':
-          if (step.hasEntered) {
-            toggleClasses('.page-1 .footer .footer-logos', ['fade-in'], ['fade-out']);
-          } else {
-            toggleClasses('.page-1 .footer .footer-logos', [], ['fade-out']);
-            step.hasEntered = true;
+        // Handlers for city names.
+        case 'step-1-last':
+        case 'step-3-last': {
+          // Extract the step number.
+          const pageNumber = step.id.split('-')[1];
+
+          if (step.id === 'step-1-last') {
+            toggleClasses('.page-1 .footer', ['fixed-content']);
+          }
+
+          if (direction === 'up') {
+            toggleClasses(`.page-${pageNumber} .city`, ['fixed-content'], []);
           }
           break;
-        case 'step-1-3':
-          toggleClasses('.page-1 .footer', ['fixed-content']);
-          toggleClasses('.page-1 .city', ['fixed-content']);
-          break;
+        }
+
         case 'step-6-1':
           toggleClasses('.page-5 #step-5-4', [], ['fixed']);
           break;
+
         default:
-          toggleClasses('.page-1 .footer .footer-logos', ['fade-out'], ['fade-in']);
           break;
       }
     },
     onStepExit: ({ element: step, direction }) => {
       switch (step.id) {
-        case 'step-1-3':
+        // Handlers for city names.
+        case 'step-1-last':
+        case 'step-3-last': {
+          const pageNumber = step.id.split('-')[1];
+
           if (direction === 'down') {
-            toggleClasses('.page-1 .city', [], ['fixed-content']);
+            toggleClasses(`.page-${pageNumber} .city`, [], ['fixed-content']);
           }
+          break;
+        }
+
+        default:
           break;
       }
     }
@@ -58,9 +70,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const scrollerTopHandlers = {
     onStepEnter: ({ element: step, direction }) => {
       switch (step.id) {
-        case 'step-1-1':
-          toggleClasses('.page-1 .city', ['fixed-content'], []);
-          toggleClasses('.page-1 .footer .footer-logos', [], ['hide']);
+        case 'step-1-first':
+          if (direction === 'down') {
+            toggleClasses('.page-1 .footer-logos', ['fade-out'], ['fade-in']);
+          }
           break;
         // For all white sections, add fixed class on exit.
         case 'step-2-1':
@@ -68,16 +81,10 @@ document.addEventListener('DOMContentLoaded', () => {
         case 'step-6-1':
         case 'step-8-1': {
           const pageNumber = step.id.split('-')[1];
-          const selector = `.page-${pageNumber} #${step.id} .left-section`;
+          const selector = `.page-${pageNumber} #${step.id} .icon`;
           toggleClasses(selector, ['fixed']);
           break;
         }
-        case 'step-3-1':
-          toggleClasses('.page-3 .city', ['fixed-content'], []);
-          break;
-        case 'step-3-2':
-          toggleClasses('.page-3 .city', [], ['fixed-content']);
-          break;
         case 'step-5-3':
           if (direction === 'down') toggleClasses('.page-5 #step-5-4', ['fixed']);
           break;
@@ -95,8 +102,10 @@ document.addEventListener('DOMContentLoaded', () => {
           toggleClasses(selector, [], ['fixed']);
           break;
         }
-        case 'step-3-1':
-          toggleClasses('.page-3 .city', [], ['fixed-content']);
+        case 'step-1-first':
+          if (direction === 'up') {
+            toggleClasses('.page-1 .footer-logos', ['fade-in'], ['fade-out']);
+          }
           break;
       }
     }
@@ -106,9 +115,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const scrollerMiddleHandlers = {
     onStepEnter: ({ element: step, direction }) => {
       switch (step.id) {
-        case 'step-3-1':
-          toggleClasses('.page-3 .city', ['fixed-content']);
+        // Handlers for city names.
+        case 'step-1-first':
+        case 'step-3-first': {
+          const pageNumber = step.id.split('-')[1];
+
+          if (direction === 'down') {
+            toggleClasses(`.page-${pageNumber} .city`, ['fixed-content']);
+          }
           break;
+        }
         case 'step-7-1':
           toggleClasses('#step-7-1 div:nth-child(3)', ['animate-img'], ['visibility-hidden']);
           setTimeout(() => {
@@ -136,12 +152,20 @@ document.addEventListener('DOMContentLoaded', () => {
             toggleClasses('.page-9 #step-9-1 h2', ['fade-out'], ['fade-in']);
           }
           break;
+        case 'step-3-first':
+        case 'step-1-first': {
+          const pageNumber = step.id.split('-')[1];
+          if (direction === 'up') {
+            toggleClasses(`.page-${pageNumber} .city`, [], ['fixed-content']);
+          }
+          break;
+        }
       }
     }
   };
 
   // Initialize scrollers
-  setupScrollama('.scroller_bottom', 0.99, scrollerBottomHandlers);
-  setupScrollama('.scrollama_top', 0.05, scrollerTopHandlers);
+  setupScrollama('.scroller_bottom', 1, scrollerBottomHandlers);
+  setupScrollama('.scrollama_top', 0, scrollerTopHandlers);
   setupScrollama('.scroller_middle', 0.5, scrollerMiddleHandlers);
 });
